4802525bf42753e0b5fdb60025a01a15e12f3bbd
Siyu Long <i@longern.com>
2022-09-08 07:17:17 +0000

    Add new folder button

--- functions/api/children/[[path]].ts
+++ functions/api/children/[[path]].ts
@@ -5,10 +5,12 @@ export async function onRequestGet(context) {
     const driveid = new URL(request.url).hostname.replace(/\..*/, "");
 
     const objList = await env[driveid].list({ prefix: path, delimiter: "/" });
-    const objKeys = objList.objects.map((obj) => {
-      const { key, size, uploaded, customMetadata } = obj;
-      return { key, size, uploaded, customMetadata };
-    });
+    const objKeys = objList.objects
+      .filter(obj => !obj.key.endsWith("/_$folder$"))
+      .map((obj) => {
+        const { key, size, uploaded, customMetadata } = obj;
+        return { key, size, uploaded, customMetadata };
+      });
 
     let folders = objList.delimitedPrefixes;
     if (!path) folders = folders.filter(folder => folder !== "_$flaredrive$/")
