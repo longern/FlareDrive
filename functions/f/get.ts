8cf5d10243306089366d44de7256bdadcc52d8b3
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-11-17 01:19:09 +0500

    Changes to cors

--- functions/f/get.ts
+++ functions/f/get.ts
@@ -6,6 +6,15 @@ export async function handleRequestGet({
   path,
   request,
 }: RequestHandlerParams) {
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "HEAD, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+      },
+    });
+  }
   const obj = await bucket.get(path, {
     onlyIf: request.headers,
     range: request.headers,
@@ -16,7 +25,14 @@ export async function handleRequestGet({
 
   const headers = new Headers();
   obj.writeHttpMetadata(headers);
+
   if (path.startsWith("_$flaredrive$/thumbnails/"))
     headers.set("Cache-Control", "max-age=31536000");
+
+  // Allow CORS for all origins
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "*");
+
   return new Response(obj.body, { headers });
 }
--- functions/f/head.ts
+++ functions/f/head.ts
@@ -4,11 +4,27 @@ import { RequestHandlerParams } from "./utils";
 export async function handleRequestHead({
   bucket,
   path,
+  request,
 }: RequestHandlerParams) {
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "HEAD, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+      },
+    });
+  }
+
   const obj = await bucket.head(path);
   if (obj === null) return notFound();
 
   const headers = new Headers();
   obj.writeHttpMetadata(headers);
+
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "HEAD, OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "*");
+
   return new Response(null, { headers });
 }
